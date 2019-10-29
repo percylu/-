f2d0d8c812a25f8b3d7b0a32d2fdcaa25706a13a
percylu <35738692@qq.com>
2019-10-29 19:51:19 +0800

    初始化代码

---
+++ addons/alisms/config.php
@@ -0,0 +1,83 @@
+<?php
+
+return array (
+  0 => 
+  array (
+    'name' => 'key',
+    'title' => '应用key',
+    'type' => 'string',
+    'content' => 
+    array (
+    ),
+    'value' => 'LTAI4FphaHxe3BMMeKtaqfZg',
+    'rule' => 'required',
+    'msg' => '',
+    'tip' => '',
+    'ok' => '',
+    'extend' => '',
+  ),
+  1 => 
+  array (
+    'name' => 'secret',
+    'title' => '密钥secret',
+    'type' => 'string',
+    'content' => 
+    array (
+    ),
+    'value' => 'WNgu2cIjRYVLdUTFA0gQP1CU3V1Z6j',
+    'rule' => 'required',
+    'msg' => '',
+    'tip' => '',
+    'ok' => '',
+    'extend' => '',
+  ),
+  2 => 
+  array (
+    'name' => 'sign',
+    'title' => '签名',
+    'type' => 'string',
+    'content' => 
+    array (
+    ),
+    'value' => '广东装备协会',
+    'rule' => 'required',
+    'msg' => '',
+    'tip' => '',
+    'ok' => '',
+    'extend' => '',
+  ),
+  3 => 
+  array (
+    'name' => 'template',
+    'title' => '短信模板',
+    'type' => 'array',
+    'content' => 
+    array (
+    ),
+    'value' => 
+    array (
+      'auth' => 'SMS_176537847',
+      'signsucss' => 'SMS_176537851',
+    ),
+    'rule' => 'required',
+    'msg' => '',
+    'tip' => '',
+    'ok' => '',
+    'extend' => '',
+  ),
+  4 => 
+  array (
+    'name' => '__tips__',
+    'title' => '温馨提示',
+    'type' => 'string',
+    'content' => 
+    array (
+    ),
+    'value' => '应用key和密钥你可以通过 https://ak-console.aliyun.com/?spm=a2c4g.11186623.2.13.fd315777PX3tjy#/accesskey 获取',
+    'rule' => 'required',
+    'msg' => '',
+    'tip' => '',
+    'ok' => '',
+    'extend' => '',
+  ),
+);
