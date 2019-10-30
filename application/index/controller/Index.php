21e79b47e71320119b842d98b285c8020f296655
percylu <35738692@qq.com>
2019-10-30 17:34:10 +0800

    报名

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -73,6 +73,32 @@ class Index extends Frontend
         return $this->view->fetch();
     }
 
+    public function signed(){
+        $data['type']=input('post.type');
+        $data['isjoin']=input('post.exhibitor');
+        $data['company']=input('post.company');
+        $data['identification']=input('post.identification');
+        $data['address']=input('post.address');
+        $data['name']=input('post.name');
+        $data['sex']=input('post.gender');
+        $data['department']=input('post.department');
+        $data['position']=input('post.position');
+        $data['mobile']=input('post.phone');
+        $data['personid']=input('post.personid');
+        $time=time();
+        $data['createtime']=$time;
+        $result= Db::table('zb_sign')->insert($data);
+
+        if($result){
+            //设置成功后跳转页面的地址，默认的返回页面是$_SERVER['HTTP_REFERER']
+            return $this->success('报名已提交', 'Index/index');
+        } else {
+            //错误页面的默认跳转页面是返回前一页，通常不需要设置
+            return $this->error('新增失败');
+        }
+
+    }
+
     public function successful()
     {
         return $this->view->fetch();
